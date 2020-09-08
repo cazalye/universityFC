c860c2998ce5989d62feb9dc57057fc3591ae292
Emma Cazalye <e.cazaly7@gmail.com>
2020-09-08 13:53:32 +1000

    Link Contact with contentful

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,40 +1,66 @@
-import React, { Component } from 'react';
-import SolsticeCredit from '../components/solsticeCredit';
+import React from 'react';
+import { graphql } from 'gatsby'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Layout from '../components/layout';
+import Head from "../components/head";
+import SolsticeCredit from '../components/solsticeCredit';
 
-class Contact extends Component {
-    render() {
-        return (
-            <Layout>
+export const query = graphql`
+    query {
+        contentfulContact  {
+            title
+            logo{
+                title
+                file{
+                  url
+                }
+              }
+            featureImage{
+                file{
+                  url
+                }
+                title
+              }
+            content{
+            json
+            }
+        }
+    }
+`
+
+const Contact = (props) => {
+    // const options = {
+    //     renderNode: {
+    //         "embedded-asset-block": (node) => {
+    //             const alt = node.data.target.fields.title['en-US']
+    //             const url = node.data.target.fields.file['en-US'].url
+    //             return <img alt={alt} src={url} />
+    //         }
+    //     }
+    // }
+
+    return (
+        <Layout>
+            <Head title = "Contact"/>
             <div className="page-container">
                 <a id="contact"/>
-            <div className="page-header">
-                <img src="./img/uni_logo_crop.png" alt="University of Tasmania logo with lion" className="utas-logo" width="180px"/>
-                <h1 className="page-title">Contact</h1>
+                <div className="page-header">
+                    <img src={props.data.contentfulContact.logo.file.url} alt={props.data.contentfulContact.logo.title} className="utas-logo" width="180px"/>
+                    <h1 className="page-title">{props.data.contentfulContact.title}</h1>
+                </div>
+                <div className="page-banner">
+                    <img className="banner-image" src={props.data.contentfulContact.featureImage.file.url} alt={props.data.contentfulContact.featureImage.title} />
+                    <SolsticeCredit/>
+                </div>
+                <div className="page-content">
+                    <h2>Contact Us</h2>
+                    <div className="contact-content">{documentToReactComponents(props.data.contentfulContact.content.json)}</div>
+                    
+                </div>
             </div>
-            <div className="page-banner">
-                <img className="banner-image" src="./img/contact.jpg" alt="Our club secretary Toby" />
-                <SolsticeCredit/>
-            </div>
-            <div className="page-content">
-                <h2>Contact Us</h2>
-                <h4 className="contact-name"><strong>Rowan Dix</strong> – Chairperson 
-                    <a href="mailto: utassc.chairperson@gmail.com" target="_blank" rel="noopener noreferrer"> utassc.chairperson@gmail.com </a>
-                </h4>
-                <h4 className="contact-name"><strong>Ben Sanderson </strong>– Vice-Chairperson
-                    <a href="mailto: utassc.vicechair@gmail.com" target="_blank" rel="noopener noreferrer"> utassc.vicechair@gmail.com </a>
-                </h4>
-                <h4 className="contact-name"><strong>Toby MacGregor</strong> – Secretary
-                    <a href="mailto: utassc.secretary@gmail.com" target="_blank" rel="noopener noreferrer"> utassc.secretary@gmail.com </a>
-                </h4>
-                <h4  className="contact-name"><strong>General Information</strong>
-                    <a href="mailto: utas.soccer@gmail.com" target="_blank" rel="noopener noreferrer"> utas.soccer@gmail.com</a>
-                </h4>
-            </div>
-        </div>
         </Layout>
-        );
-    }
+    )
 }
 
-export default Contact;
+export default Contact
+
