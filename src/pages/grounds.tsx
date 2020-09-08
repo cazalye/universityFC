e994b79ea620d95f53d88d89d07a888083625549
Emma Cazalye <e.cazaly7@gmail.com>
2020-09-09 08:57:45 +1000

    Add Grounds content to contentful

--- src/pages/grounds.tsx
+++ src/pages/grounds.tsx
@@ -2,27 +2,50 @@ import React, { Component } from 'react';
 import '../styles/pages.scss';
 import SolsticeCredit from '../components/solsticeCredit';
 import Layout from '../components/layout';
+import { graphql } from 'gatsby'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Head from "../components/head";
 
-export default class Grounds extends Component {
-    render() {
+export const query = graphql`
+    query {
+        contentfulGrounds  {
+            title
+            logo{
+                title
+                file{
+                  url
+                }
+              }
+            featureImage{
+                file{
+                  url
+                }
+                title
+              }
+            content{
+                json
+            }
+        }
+    }
+`
+
+const Grounds = (props) => {
         return (
             <Layout>
-            <div className="page-container">
-            <a id="grounds"/>
-            <div className="page-header">
-                <img src="./img/uni_logo_crop.png" alt="University of Tasmania logo with lion" className="utas-logo" width="180px"/>
-                <h1 className="page-title">Grounds</h1>
-            </div>
-            <div className="page-banner">
-                <img className="banner-image" src="./img/olinda.jpg" alt="One of our senior men taking a throw in at our home ground Olinda Grove" />
-                <SolsticeCredit/>
-            </div>
-            <div className="page-content">
-                <h2>Olinda Grove - The Hive</h2>
-                <p className="page-description">Our home grounds located in Olinda Grove Mount Nelson are fondly dubbed 'The Hive' as the home of the Uni Bees. Here, we have three grounds where we train on Tuesdays and Thursdays. <hr/>We have clubrooms with showers and an upstairs indoor/outdoor bar and kiosk area- a great spot to watch our home games.</p>
-            </div>
-        </div>
+                <Head title = "Grounds"/>
+                <div className="page-container">
+                    <a id="grounds"/>
+                    <div className="page-header">
+                        <img src={props.data.contentfulGrounds.logo.file.url} alt={props.data.contentfulGrounds.logo.title} className="utas-logo" width="180px"/>
+                        <h1 className="page-title">{props.data.contentfulGrounds.title}</h1>
+                    </div>
+                    <div className="page-banner">
+                        <img className="banner-image" src={props.data.contentfulGrounds.featureImage.file.url} alt={props.data.contentfulGrounds.featureImage.title} />
+                        <SolsticeCredit/>
+                    </div> 
+                    <div className="grounds-content">{documentToReactComponents(props.data.contentfulGrounds.content.json)}</div>
+                </div>
         </Layout>
         )
     }
-}
+export default Grounds
