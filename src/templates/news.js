9d7af01667edf6b144640c4c415e49a68789f203
Emma Cazalye <e.cazaly7@gmail.com>
2020-09-10 17:19:57 +1000

    Restore news pages as throwing error

--- src/templates/news.js
+++ src/templates/news.js
@@ -1,41 +1,41 @@
-// import React from 'react'
-// import { graphql } from 'gatsby'
-// import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
-// import Head from "../components/head"
+import React from 'react'
+import { graphql } from 'gatsby'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Head from "../components/head"
 
 
-// export const query = graphql`
-//     query($slug: String!){
-//         contentfulNews(slug: {eq: $slug}) {
-//             title
-//             date(formatString:"MMMM Do, YYYY")
-//             content {
-//                 json
-//             }
-//         }
-//     }
-// `
+export const query = graphql`
+    query($slug: String!){
+        contentfulNews(slug: {eq: $slug}) {
+            title
+            date(formatString:"MMMM Do, YYYY")
+            content {
+                json
+            }
+        }
+    }
+`
 
-// const News = (props) => {
-//     const options = {
-//         renderNode: {
-//             "embedded-asset-block": (node) => {
-//                 const alt = node.data.target.fields.title['en-US']
-//                 const url = node.data.target.fields.file['en-US'].url
-//                 return <img alt={alt} src={url} />
-//             }
-//         }
-//     }
+const News = (props) => {
+    const options = {
+        renderNode: {
+            "embedded-asset-block": (node) => {
+                const alt = node.data.target.fields.title['en-US']
+                const url = node.data.target.fields.file['en-US'].url
+                return <img alt={alt} src={url} />
+            }
+        }
+    }
 
-//     return (
-//         <div>
-//             <Head title={props.data.contentfulNews.title}/>
-//             <h1>{props.data.contentfulNews.title}</h1>
-//             <p>{props.data.contentfulNews.date}</p>
-//             {documentToReactComponents(props.data.contentfulNews.content.json, options)}
-//         </div>
+    return (
+        <div>
+            <Head title={props.data.contentfulNews.title}/>
+            <h1>{props.data.contentfulNews.title}</h1>
+            <p>{props.data.contentfulNews.date}</p>
+            {documentToReactComponents(props.data.contentfulNews.content.json, options)}
+        </div>
 
-//     )
-// }
+    )
+}
 
-// export default News
+export default News
