4bea672a312c83394a5f75ded3dbb764972c76bb
Emma Cazalye <e.cazaly7@gmail.com>
2020-09-19 15:15:54 +1000

    Style news template

--- src/templates/news.js
+++ src/templates/news.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Head from "../components/head"
+import Layout from "../components/layout"
 
 
 export const query = graphql`
@@ -11,6 +12,12 @@ export const query = graphql`
             date(formatString:"MMMM Do, YYYY")
             content {
                 json
+            }
+            featureImage{
+                file{
+                    url
+                }
+                title
             }
         }
     }
@@ -28,13 +35,23 @@ const News = (props) => {
     }
 
     return (
-        <div>
-            <Head title={props.data.contentfulNews.title}/>
-            <h1>{props.data.contentfulNews.title}</h1>
-            <p>{props.data.contentfulNews.date}</p>
-            {documentToReactComponents(props.data.contentfulNews.content.json, options)}
-        </div>
+        <Layout>
+        <Head title={props.data.contentfulNews.title}/>
+        <div className="page-container">
+            <div className="page-header">
+                <img src="/uni_logo_crop.png" alt="University of Tasmania logo with lion" className="utas-logo" width="180px"/>
+                <h1 className="page-title">{props.data.contentfulNews.title}</h1>
+                <p>{props.data.contentfulNews.date}</p>
+            </div>
+            <div className="page-banner">
+                <img className="news-image" src={props.data.contentfulNews.featureImage.file.url} alt={props.data.contentfulNews.featureImage.title} />
+            </div>
 
+            <div className="news-content">
+                {documentToReactComponents(props.data.contentfulNews.content.json, options)}
+            </div>
+        </div>
+        </Layout>
     )
 }
 
